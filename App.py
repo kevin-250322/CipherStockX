3f7c35db7d672d481d1885fb8942c7675088d591
Srinag Marni <51700595+msrinag@users.noreply.github.com>
2024-10-27 23:59:58 -0400

    Update App.py

--- App.py
+++ App.py
@@ -497,5 +497,5 @@ All stock data is sourced from the Yahoo Finance API, and all company logos are 
 During sentiment analysis, we scrape news and financial information from Yahoo News. However, some articles may block our scraping tools (`BeautifulSoup` and `requests`), limiting content access. Additionally, on Streamlit Cloud, we avoid using Selenium due to its high memory usage and slower performance, which could increase wait times for users.
 
             """)
-st.image('Assets/ideas.gif')
+st.image('https://raw.githubusercontent.com/msrinag/CipherStockX/refs/heads/main/Assets/ideas.gif')
 
